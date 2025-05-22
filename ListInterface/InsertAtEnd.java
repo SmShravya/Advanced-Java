106004da5c4cb7a8bbcb4dc93640e02b55788cb6
SHRAVYA S M <154530282+SmShravya@users.noreply.github.com>
2025-05-22 10:38:20 +0530

    Add files via upload

---
+++ ListInterface/InsertAtEnd.java
@@ -0,0 +1,24 @@
+
+//3. Write a Java program to insert the specified element at the end of a linked list.( using
+//l_listobj.offerLast("Pink"))
+package listInterface;
+import java.util.*;
+public class InsertAtEnd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<String> ls = new LinkedList<>();
+		ls.add("Orange");
+		ls.add("geen");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+		System.out.println(ls);
+		ls.addLast("pink");
+		//add element at the last
+		System.out.println(ls);
+	
+	}
+
+}
